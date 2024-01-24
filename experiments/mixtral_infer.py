a20ce4db60d40170fba48d8d1ce55cdaf88ead2b
BoyuanJackChen <bc3194@nyu.edu>
2024-01-24 02:26:28 -0500

    Updated inference pipeline

--- experiments/mixtral_infer.py
+++ experiments/mixtral_infer.py
@@ -1,30 +1,67 @@
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, AutoModelForCausalLM
 import transformers
 import torch
 
-model = "mistralai/Mixtral-8x7B-v0.1"
+model_name = "mistralai/Mixtral-8x7B-v0.1"
+model_name = "mistralai/Mixtral-8x7B-Instruct-v0.1"
 
-tokenizer = AutoTokenizer.from_pretrained(model)
+# Need to convert the prompt to alpaca instruction format because the model is instruction-tuned
+def alpaca_prompt(input):
+    INSTRUCTION = f"""Below is an instruction that describes a task. Write a response that appropriately completes the request.
 
-pipeline = transformers.pipeline(
-    "text-generation",
-    model=model,
-    torch_dtype=torch.float16,
-    device_map="auto",
-)
 
-prompt = open('./questions/whataxor.txt', 'r').read()
-# prompt = "Write a hello world function in python.\n"
-sequences = pipeline(
-    prompt,
-    do_sample=True,
-    top_k=10,
-    temperature=0.1,
-    top_p=0.95,
-    num_return_sequences=1,
-    eos_token_id=tokenizer.eos_token_id,
-    pad_token_id=tokenizer.eos_token_id,
-    max_new_tokens=300,
-)
-for seq in sequences:
-    print(f"Result: {seq['generated_text'][len(prompt):]}")
+### Instruction:
+{input}
+
+### Response:"""
+    return INSTRUCTION
+
+
+
+def main():
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        load_in_8bit=False,
+        torch_dtype=torch.float16,
+        device_map="auto")
+    model.eval()
+    
+    pass_at = 1
+    prompt_file = './prompts_open/rev/baby\'s_third.txt'
+    prompt = open(prompt_file, 'r').read()
+    while prompt != "":
+        prompt = prompt.replace('    ', '\t')
+        prompt = alpaca_prompt(prompt)
+        prompt_ids = tokenizer.batch_encode_plus([prompt]*max(pass_at,1), return_tensors="pt", truncation=True, max_length=2048).to(torch.cuda.current_device())
+                
+        max_new_tokens = 1024
+        with torch.no_grad():
+            answer_ids = model.generate(
+                **prompt_ids,
+                use_cache = True,
+                pad_token_id = tokenizer.eos_token_id,
+                eos_token_id = tokenizer.eos_token_id,
+                max_new_tokens = max_new_tokens,
+                do_sample = True,
+                top_k = 0,
+                top_p = 0.95,
+                temperature = 0.8,
+                num_beams = 1
+            )
+        answer_ids_trimmed = answer_ids[:, len(prompt_ids['input_ids'][0]):]
+        answer_text_trimmed = tokenizer.batch_decode(answer_ids_trimmed, skip_special_tokens=True)
+        answer_text = tokenizer.batch_decode(answer_ids, skip_special_tokens=True)
+        torch.cuda.empty_cache()
+        for answer in answer_text_trimmed:
+            print(f"{answer}")
+        print(f"\n\nAbove is the trimmed answer for {prompt_file}. Are you satisfied with the answer? If not, Please enter new prompt or the prompt .txt file address:")
+        given_input = input()
+        if given_input.endswith(".txt"):
+            prompt = open(given_input, 'r').read()
+        else:
+            prompt = given_input
+
+
+if __name__=="__main__":
+    main()
--- experiments/wizardcoder_infer.py
+++ experiments/wizardcoder_infer.py
@@ -1,14 +1,10 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
-from transformers import StoppingCriteria, StoppingCriteriaList
-from transformers import LogitsProcessor, LogitsProcessorList
 import transformers
 import torch
 
 model_name = "WizardLM/WizardCoder-Python-34B-V1.0"
 
-stop_words = ["\n#", "\n```\n"]
-stop_words_ids = [[13,29937], [13,28956,13], [13,28956,30004]]
-
+# Need to convert the prompt to alpaca instruction format because the model is instruction-tuned
 def alpaca_prompt(input):
     INSTRUCTION = f"""Below is an instruction that describes a task. Write a response that appropriately completes the request.
 
@@ -29,33 +25,10 @@ def main():
         torch_dtype=torch.float16,
         device_map="auto")
     model.eval()
-
-    class StopSequences(LogitsProcessor):
-        def __init__(self, stop_ids, batch_size, encounters=1, eos_token_id=2):
-            StoppingCriteria.__init__(self)
-            self.stop_sequences = stop_ids
-            self.batch_size = batch_size
-            self.encounters = [encounters] * batch_size
-            self.NUM_ENCOUNTERS = encounters
-            self.eos_token_id = eos_token_id
-
-        def __call__(self, input_ids, scores):
-            forced_eos = torch.full((scores.size(1),), -float("inf"))
-            forced_eos[self.eos_token_id] = 0
-            for stop in self.stop_sequences:
-                # Check if the input_ids end with the stop sequence
-                for i in range(self.batch_size):
-                    if self.encounters[i] <= 0:
-                        continue
-                    if input_ids[i][-len(stop):].tolist() == stop:
-                        self.encounters[i] -= 1
-                        if self.encounters[i] <= 0:
-                            scores[i] = forced_eos
-            return scores
     
     pass_at = 1
-    logits_processor = LogitsProcessorList([StopSequences(stop_words_ids, batch_size=max(pass_at,1), encounters=1)])
-    prompt = open('./questions_open/target_practice.txt', 'r').read()
+    prompt_file = './prompts_open/rev/baby\'s_third.txt'
+    prompt = open(prompt_file, 'r').read()
     while prompt != "":
         prompt = prompt.replace('    ', '\t')
         prompt = alpaca_prompt(prompt)
@@ -74,20 +47,19 @@ def main():
                 top_p = 0.95,
                 temperature = 0.8,
                 num_beams = 1
-                # logits_processor = logits_processor
             )
         answer_ids_trimmed = answer_ids[:, len(prompt_ids['input_ids'][0]):]
         answer_text_trimmed = tokenizer.batch_decode(answer_ids_trimmed, skip_special_tokens=True)
         answer_text = tokenizer.batch_decode(answer_ids, skip_special_tokens=True)
         torch.cuda.empty_cache()
-        print(f"The trimmed answer is:\n")
         for answer in answer_text_trimmed:
             print(f"{answer}")
-        # print(f"The original answer is:\n")
-        # for answer in answer_text:
-        #     print(f"{answer}")
-        print(f"\n\nAre you satisfied with the answer? If not, Please enter new prompt:")
-        prompt = input()
+        print(f"\n\nAbove is the trimmed answer for {prompt_file}. Are you satisfied with the answer? If not, Please enter new prompt or the prompt .txt file address:")
+        given_input = input()
+        if given_input.endswith(".txt"):
+            prompt = open(given_input, 'r').read()
+        else:
+            prompt = given_input
 
 
 if __name__=="__main__":
