f7d8cca7cfd7b8b5441813ea27445642e0eab50a
NickNameInvalid <zssmh98@outlook.com>
2024-01-28 01:50:55 +0400

    add mixtral framework

---
+++ llm_ctf/models/mixtral.py
@@ -0,0 +1,25 @@
+from openai import OpenAI
+from .base_model import BaseModel
+
+class Mixtral8x7B(BaseModel):
+    def __init__(self, model_id: str, url_path):
+        self.super(Mixtral8x7B, self).__init__(model_id, url_path, None)
+        self.client = OpenAI(
+            api_key=self.key,
+            base_url=self.url
+        )
+        self.prompts = []
+        
+    def generate(self, prompt, use_file=True, temperature=0.8, top_p=0.9):
+        self.prompts.append({"role": "user", "content": prompt})
+        
+        if use_file:
+            with open(prompt, 'r') as f:
+                prompt = f.read()
+        
+        resp = self.client.chat.completions.create(
+            model=self.model_id,
+            messages=[self.prompts],
+        )
+        self.prompts.append({"role": "assistant", "content": resp.choices[0].message.content})
+        return resp.choices[0].message.content
