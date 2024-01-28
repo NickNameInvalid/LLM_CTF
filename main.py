c694022ebed863d8a2c6a67a2a037bbb23d0f66a
Minghao Shao <40309309+NickNameInvalid@users.noreply.github.com>
2024-01-28 17:09:37 -0500

    update

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import llm_ctf.mixtral_task as mixtral
 
 def main():
     
-    mixtral_task = mixtral.MixtralTask(question_path="/home/ms12416/projects/LLM_CTF/questions/rev/rebug 1", 
-                                       task_name="rebug 1",
-                                       url_path="/home/ms12416/projects/LLM_CTF/keys/mixtral_api.txt")
-    resp = mixtral_task.task_prompt(prompt="/home/ms12416/projects/LLM_CTF/experiments/prompts_open/rev/rebug1.txt", use_file=True)
+    mixtral_task = mixtral.MixtralTask(question_path="/Users/minghaoshao/Documents/LLM_CTF/questions/misc/linear_aggressor", 
+                                       task_name="linear_aggressor",
+                                       url_path="/Users/minghaoshao/Documents/LLM_CTF/keys/mixtral_api.txt")
+    resp = mixtral_task.task_prompt(prompt="/Users/minghaoshao/Documents/LLM_CTF/experiments/prompts_open/misc/linear_aggressor.txt", use_file=True)
     print("============================== RESPONSE FROM MODEL ==============================")
     print(resp)
     mixtral_task.save_code(resp)
