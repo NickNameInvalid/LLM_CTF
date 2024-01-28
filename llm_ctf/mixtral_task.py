043fc429690f59153b8c7e4db215feaeb1d79273
NickNameInvalid <zssmh98@outlook.com>
2024-01-27 23:08:46 -0500

    update framework with automatic validation

--- llm_ctf/mixtral_task.py
+++ llm_ctf/mixtral_task.py
@@ -1,37 +1,65 @@
 import re
 import os
+import subprocess
 from .models.mixtral import Mixtral8x7B
 
 class MixtralTask:
-    def __init__(self) -> None:
-        self.task_name=""
+    def __init__(self, question_path: str, task_name: str, url_path: str) -> None:
+        self.task_name= task_name
+        self.question_path = question_path
+        self.model = self.mixtral_init(url_path)
+        self.valid = False
+        
+    def mixtral_init(self, url_path):
+        with open(url_path, 'r') as f:
+            url = f.read()
+        
+        model = Mixtral8x7B(
+            model_id="mistralai/Mixtral-8x7B-Instruct-v0.1",
+            url_path=url
+        )
+        return model
     
-    def evaluate_result():
-        pass
+    def extract_python_code(self, text):
+        pattern = r'```python\n(.*?)```'
+        match = re.search(pattern, text, re.DOTALL)
+        if match:
+            return match.group(1).strip()
+        else:
+            return "Python code block not found."
+        
+    def task_prompt(self, prompt, use_file=True):
+        return self.model.generate(prompt, use_file=True)
+    
+    def save_code(self, resp_text: str, save_path: str, file_name="sol.py"):
+        code_snpt = self.extract_python_code(resp_text)
+        with open(os.path.join(save_path, file_name), 'w') as f:
+            f.write(code_snpt)
+    
+    def validate_sol(self, resp: str):
+        with open(os.path.join(self.question_path, 'flag.txt'), 'r') as f:
+            flag = f.read().split()
+        
+        if flag in resp:
+            print(f"Validation succeeded with returned flag {flag}")
+            self.valid = True
+            return self.valid
+        
+        p = subprocess.Popen('python ' + os.path.join(self.question_path, 'sol.py'), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        res = '\n'.join(p.stdout.readlines())
+        if flag in res:
+            print(f"Validation succeeded with returned flag {flag}")
+            self.valid = True
+            return self.valid
+
+        print(f"Validation failed, the answer returned is: \n {res}")
+        return self.valid
         
 
-def extract_python_code(text):
-    pattern = r'```python\n(.*?)```'
-    match = re.search(pattern, text, re.DOTALL)
-    if match:
-        return match.group(1).strip()
-    else:
-        return "Python code block not found."
 
-def mixtral_init(url_path):
-    with open(url_path, 'r') as f:
-        url = f.read()
-    
-    model = Mixtral8x7B(
-        model_id="mistralai/Mixtral-8x7B-Instruct-v0.1",
-        url_path=url
-    )
-    return model
 
-def task_prompt(model, prompt, use_file=True):
-    return model.generate(prompt, use_file=True)
 
-def save_code(resp_text: str, save_path: str, file_name="sol.py"):
-    code_snpt = extract_python_code(resp_text)
-    with open(os.path.join(save_path, file_name), 'w') as f:
-        f.write(code_snpt)
+
+
+
+        
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import llm_ctf.mixtral_task as mixtral
 
 def main():
-    model = mixtral.mixtral_init(url_path="/home/ms12416/projects/LLM_CTF/keys/mixtral_api.txt")
-    resp = mixtral.task_prompt(model, prompt="/home/ms12416/projects/LLM_CTF/experiments/prompts_open/rev/rebug1.txt", use_file=True)
-    mixtral.save_code(resp, save_path="/home/ms12416/projects/LLM_CTF/questions/rev/rebug 1")
+    
+    mixtral_task = mixtral.MixtralTask(question_path="/home/ms12416/projects/LLM_CTF/questions/rev/rebug 1", 
+                                       task_name="rebug 1",
+                                       url_path="/home/ms12416/projects/LLM_CTF/keys/mixtral_api.txt")
+    resp = mixtral_task.task_prompt(prompt="/home/ms12416/projects/LLM_CTF/experiments/prompts_open/rev/rebug1.txt", use_file=True)
     print(resp)
+    mixtral_task.save_code(resp, save_path="/home/ms12416/projects/LLM_CTF/questions/rev/rebug 1")
+    mixtral_task.validate_sol(resp)
     
 if __name__ == "__main__":
     main()
