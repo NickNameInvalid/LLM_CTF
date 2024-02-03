9dfacc7ee25af2fec05d8258b8c5fd01e2b003b4
sj2790 <sj2790@nyu.edu>
2024-02-02 19:06:21 -0500

    Dockerization

--- llm_ctf/utils/dockertool.py
+++ llm_ctf/utils/dockertool.py
@@ -1,15 +1,24 @@
+import subprocess
 class DockerHelper:
     def __init__(self, container_name):
         self.container = container_name
     
     def docker_start(self):
         raise NotImplementedError
-    
+
     def docker_stop(self):
         raise NotImplementedError
     
     def docker_copy(self, docker_id, src, dest):
         raise NotImplementedError
     
-    def docker_exec(self):
-        raise NotImplementedError
+    def docker_exec(self, cmd):
+        subprocess.run(['sudo', 'docker', 'run','--rm', '-it', '-v', '$PWD:/opt/exp', self.container, cmd], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, timeout=5)
+        p = subprocess.run(['sudo', 'docker', 'run','--rm', '-it', '-v', '$PWD:/opt/exp', self.container, cmd], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, timeout=5)
+        res: str = str('\n' + p.stdout.decode("utf-8"))
+
+
+
+
+
+
