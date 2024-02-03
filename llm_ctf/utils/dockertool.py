09164cc8d8f1ff7adc1c206a3ac6e474c45d13ed
sj2790 <sj2790@nyu.edu>
2024-02-02 19:17:12 -0500

    Docker stuff

--- llm_ctf/utils/dockertool.py
+++ llm_ctf/utils/dockertool.py
@@ -13,9 +13,15 @@ class DockerHelper:
         raise NotImplementedError
     
     def docker_exec(self, cmd):
-        subprocess.run(['sudo', 'docker', 'run','--rm', '-it', '-v', '$PWD:/opt/exp', self.container, cmd], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, timeout=5)
-        p = subprocess.run(['sudo', 'docker', 'run','--rm', '-it', '-v', '$PWD:/opt/exp', self.container, cmd], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, timeout=5)
-        res: str = str('\n' + p.stdout.decode("utf-8"))
+        # subprocess.run(['sudo', 'docker', 'run','--rm', '-it', '-v', '$PWD:/opt/exp', self.container, cmd], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, timeout=5)
+        # p = subprocess.run(['sudo', 'docker', 'run','--rm', '-it', '-v', '$PWD:/opt/exp', self.container, cmd], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, timeout=5)
+        # res: str = str('\n' + p.stdout.decode("utf-8"))
+        try:
+            p = subprocess.run(['sudo', 'docker', 'run','--rm', '-it', '-v', '$PWD:/opt/exp', self.container, cmd], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, timeout=5)
+            res: str = str('\n' + p.stdout.decode("utf-8"))
+        except Exception as e:
+            print(f"Validation failed, solver cannot be executed or solver execution error")
+            return self.valid
 
 
 
