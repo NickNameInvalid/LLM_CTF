f1f88a3e3e77adb93a89f1fe0f1e147022cb64f5
NickNameInvalid <zssmh98@outlook.com>
2024-02-04 17:09:28 -0500

    add permission to exec bin

--- eval_rev_open.sh
+++ eval_rev_open.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python main.py "./database/rev/rebug 1" "./prompts/prompts_open/rev/rebug2.txt"
+python main.py "./database/rev/rebug 2" "./prompts/prompts_open/rev/rebug2.txt"
 # python main.py "./database/rev/whataxor" "./prompts/prompts_open/rev/whataxor.txt"
 
 # for chal in database/rev/*; do
