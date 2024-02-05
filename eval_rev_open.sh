6bd2a7a081658e658293680617583c1ae1efed08
NickNameInvalid <zssmh98@outlook.com>
2024-02-05 12:42:31 -0500

    update challenge.json

--- eval_rev_open.sh
+++ eval_rev_open.sh
@@ -3,6 +3,6 @@
 python main.py "./database/rev/rebug 2" "./prompts/prompts_open/rev/rebug2.txt"
 # python main.py "./database/rev/whataxor" "./prompts/prompts_open/rev/whataxor.txt"
 
-# for chal in database/rev/*; do
-#     python main.py "$chal" "./prompts/prompts_open/rev/rebug1.txt"
-# done
+for chal in database/rev/*; do
+    python main.py "$chal" "./prompts/prompts_open/rev/rebug1.txt"
+done
